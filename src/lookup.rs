70fc41679ff0809aaca5d57abbf95679fa85e9c0
Corey Brown <coreyabrown.cb@gmail.com>
2021-02-19 16:12:10 -0600

    Extract ID - Checkpoint

--- src/lookup.rs
+++ src/lookup.rs
@@ -1,4 +1,3 @@
-//use twilight_embed_builder::{EmbedBuilder, EmbedFieldBuilder};
 use twilight_http::Client as HttpClient;
 use twilight_model::gateway::{payload::MessageCreate};
 use twilight_model::channel::embed::{Embed, EmbedField};
@@ -15,44 +14,46 @@ pub async fn lookup(http: &HttpClient, msg: &Box<MessageCreate>, keyword: String
     } else if &search_results.len() == &1 {
         //Exact match! Start building the embed and send a response
         println!("Exact match! {:?}", &search_results);
+        let id = extract_id(&search_results).await?;
+        let embed = build_embed(&id).await?;
+        http.create_message(msg.channel_id).reply(msg.id).embed(embed)?.await?;
     } else {
         //Disambiguous. Ask user which of the short list they mean.
+        //TODO: Ask User which result they mean. Then start making the embed after that
         println!("A lot of matches {:?}", &search_results);
+        let embed = build_embed(&"8461").await?;
+        http.create_message(msg.channel_id).reply(msg.id).embed(embed)?.await?;
     }
-    let embed = Embed {
-        author: None,
-        color: Some(12009742),
-        description: Some("Here is a description".to_owned()),
-        fields: vec![EmbedField {
-            inline: true,
-            name: keyword,
-            value: "You looked up a keyword!".to_owned()
-            },
-            EmbedField {
-            inline:true,
-            name: "Static Name".to_owned(),
-            value: "Static Value".to_owned()
-            }
-        ],
-        footer: None,
-        image: None,
-        kind: "rich".to_owned(),
-        provider: None,
-        thumbnail: None,
-        timestamp: None,
-        title: Some("Title".to_owned()),
-        url: Some("https://pf2.easytool.es/".to_owned()),
-        video: None        
-    };
-    http.create_message(msg.channel_id).reply(msg.id).embed(embed)?.await?;
     Ok(())
+}
+
+/**
+ * This method splits a result and extracts the id to be used later
+ **/
+async fn extract_id(result: &Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
+    let slice = &result[0];
+    let start_b_slice = slice.find(">").unwrap()+1;
+    let mut id = String::with_capacity(10); 
+    id = slice[start_b_slice..].to_string();
+    return Ok(id)
+}
+
+/**
+ * This method splits a result and extracts the title and subtitle to be used later
+ **/
+ async fn extract_info(result: &Vec<String>) -> Result<String, Box<dyn std::error::Error>> {
+    let slice = &result[0];
+    let end_b_slice = slice.find(">").unwrap();
+    let mut info = String::with_capacity(10); 
+    info = slice[..end_b_slice].to_string();
+    return Ok(info)
 }
 
 /**
  * This method searches for a term, then adds the top 3 results to a vector in the form of
  * ["Prescient Planner - GENERAL FEAT 3 > 8462", "Prescient Consumable - GENERAL FEAT 7 > 8461"]
  **/
-pub async fn search_for_term(term: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
+async fn search_for_term(term: &str) -> Result<Vec<String>, Box<dyn std::error::Error>> {
     //This function will return a vector of search results
     let req_body:String = format!("name={}", term);
     let client = reqwest::Client::new();
@@ -83,7 +84,7 @@ pub async fn search_for_term(term: &str) -> Result<Vec<String>, Box<dyn std::err
                 let one_result_title = &r[start_bytes_title..end_bytes_title];
                 let one_result_extra = &r[start_bytes_extra..end_bytes_extra];
                 let one_result_id = &r[start_bytes_id..end_bytes_id];
-                let one_result = format!("{} - {} > {}", one_result_title, one_result_extra, one_result_id);
+                let one_result = format!("{} - {}>{}", one_result_title, one_result_extra, one_result_id);
                 //Add the result to the vector
                 search_results.push(one_result);
             }
@@ -96,4 +97,55 @@ pub async fn search_for_term(term: &str) -> Result<Vec<String>, Box<dyn std::err
         return Ok(search_results);
     }
     return Ok(search_results);
+}
+
+/**
+ * This method uses an id to find the specific result. Then builds an embed.
+ * The embed should use Title, Traits, Details, Description, and URL
+ **/
+async fn build_embed(id: &str) -> Result<Embed, Box<dyn std::error::Error>> {
+    let req_body:String = format!("id={}", id);
+    let req_url:String = format!("https://pf2.easytool.es/index.php?id={}", &id);
+    let client = reqwest::Client::new();
+    //Send the request
+    let response = client.get(&req_url)
+        .header("Content-Type", "application/x-www-form-urlencoded")
+        .body(req_body)
+        .send()
+        .await?;
+    //Check the response for a success
+    if response.status().is_success() {
+        //Split the response
+        let _response_string: String = response.text().await?;
+        //TODO: Split the response string up and extract the Title, Description, Traits, and Details
+        //println!("{:?}", response_string);
+    }
+    
+    let embed = Embed {
+        author: None,
+        color: Some(12009742),
+        description: Some("Description".to_owned()),
+        fields: vec![EmbedField {
+            inline: true,
+            name: "Traits".to_owned(),
+            value: "You looked up a keyword!".to_owned()
+            },
+            EmbedField {
+            inline:true,
+            name: "Details".to_owned(),
+            value: "Static Value".to_owned()
+            }
+        ],
+        footer: None,
+        image: None,
+        kind: "rich".to_owned(),
+        provider: None,
+        thumbnail: None,
+        timestamp: None,
+        title: Some("Title".to_owned()),
+        url: Some(req_url.to_owned()),
+        video: None        
+    };
+
+    return Ok(embed);
 }
