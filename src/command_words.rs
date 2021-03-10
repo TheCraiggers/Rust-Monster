111cfb0b49971888828acd16c0bbe993b541a19e
thecraiggers <thecraiggers@chaoslogic.net>
2021-03-10 18:22:10 -0500

    Now every command MUST have a help

--- src/command_words.rs
+++ src/command_words.rs
@@ -7,9 +7,9 @@ enum WordType {
     Target
 }
 
-struct Word<'a> {
+pub struct Word<'a> {
     kind: WordType,
-    word: &'a str,
+    pub word: &'a str,
     short_help: &'a str,
     long_help: &'a str,
 }
@@ -23,11 +23,17 @@ impl Word<'_> {
 ///////////////////////////////////////////////////////
 // VERBS 
 ///////////////////////////////////////////////////////
-const Verbs: [Word; 1] = [
+pub const VERBS: [Word; 2] = [
     Word{
         word: "help",
         kind: WordType::Verb,
         short_help: "Get help on any bot command or term",
         long_help: "Use the help command to get detailed help about any command word the bot recognizes. Which you probably already knew, since you just typed `!help help`. Clever girl.",
+    },
+    Word{
+        word: "lookup",
+        kind: WordType::Verb,
+        short_help: "Get definitions of feats, spells, rules, etc",
+        long_help: "The lookup command can look up the definitions of just about any Pathfinder thing there is, using the power of the Pathfinder 2 Easy Library. Feats, skills, spells, creatures, gods, you name it.",
     },
 ];
