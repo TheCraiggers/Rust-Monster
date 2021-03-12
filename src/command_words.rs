91f40d6f1d1d1cdcf2b0509d0dc452369042d7c0
thecraiggers <thecraiggers@chaoslogic.net>
2021-03-12 11:23:42 -0500

    Added generic help message and a test for it

--- src/command_words.rs
+++ src/command_words.rs
@@ -4,15 +4,21 @@
 use anyhow::{Result, anyhow};
 use crate::discord::{DiscordReferences};
 
+/// Respond to !help commands. If an argument is supplied, return detailed help for it, otherwise generic bot help is sent
 pub async fn handle_help_command(discord_refs: &DiscordReferences<'_>, help_term: &str) -> Result<()> {
-    for words_array in ALL_WORDS.iter() {
-        match words_array.iter().find(|&word| word.word == help_term) {
-            Some(word) => { return Ok(discord_refs.dm_help_message(&word).await?)}
-            None => { continue; }
-        };
+    if help_term.is_empty() {
+        discord_refs.send_message_reply(&generate_generic_help_message()).await?;
+        Ok(())
+    } else {
+        for words_array in ALL_WORDS.iter() {
+            match words_array.iter().find(|&word| word.word == help_term) {
+                Some(word) => { return Ok(discord_refs.dm_help_message(&word).await?)}
+                None => { continue; }
+            };
+        }
+        discord_refs.send_message_reply(format!("Could not find help for '{}'. Check your spelling.", help_term).as_str()).await?;
+        return Err(anyhow!("Could not find help for term supplied"));
     }
-    discord_refs.send_message_reply(format!("Could not find help for '{}'. Check your spelling.", help_term).as_str()).await?;
-    Err(anyhow!("Could not find help for '{}'. Check your spelling.", help_term))
 }
 
 enum WordType {
@@ -33,6 +39,18 @@ impl Word<'_> {
     pub fn embed_title(&self) -> String {
         format!("Help for {}", self.word)
     }
+}
+
+fn generate_generic_help_message() -> String {
+    let mut response: String = "Most commands take the form of `!verb noun target`, where target is usually the name of a character. The following words/commands are known to the bot. You can use `!help <word>` for more info about any of these:\n\n".to_string();
+    let verb_list: Vec<String> = VERBS.iter().map(|word| word.word.to_string()).collect();
+    let noun_list: Vec<String> = NOUNS.iter().map(|word| word.word.to_string()).collect();
+    let target_list: Vec<String> = TARGETS.iter().map(|word| word.word.to_string()).collect();
+    response.push_str(&format!("**Verbs:** {}\n", verb_list.join(", ")));
+    response.push_str(&format!("**Nouns:** {}\n", noun_list.join(", ")));
+    response.push_str(&format!("**Targets:** {}\n", target_list.join(", ")));
+
+    response
 }
 
 const ALL_WORDS: [&[Word];3] = [&VERBS,&NOUNS,&TARGETS];
@@ -88,5 +106,11 @@ mod tests {
                 assert!(word.long_help.chars().count() < 2048)
             }
         }
+    }
+
+    #[test]
+    fn generic_help_message_length() {
+        //! Discord prohibts messages from being larger than 2000 chars
+        assert!(generate_generic_help_message().chars().count() < 2000)
     }
 }
